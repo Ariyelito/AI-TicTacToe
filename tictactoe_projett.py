a2c687dc7261c8bec9ee590bc072fd4340a9eef4
commando2701 <totof2724@hotmail.com>
2022-09-29 17:20:14 -0400

    Update tictactoe_projett.py
    Projet Final tic tac toe

--- tictactoe_projett.py
+++ tictactoe_projett.py
@@ -8,15 +8,16 @@ from tkinter.messagebox import askyesno
 global grille
 grille = [[" " for x in range(3)] for y in range(3)]
 
+button = []
+moves = []
 v = 0
 joueur = 0
 ordi = 0
 ordiIA = 0
-moves = []
-global ayt 
-global essaie
+etat = "INIT"
 
 
+# vérifie les combinaisons gagnantes
 def a_gagner(b, l):
     return ((b[0][0] == l and b[0][1] == l and b[0][2] == l) or
             (b[1][0] == l and b[1][1] == l and b[1][2] == l) or
@@ -28,81 +29,104 @@ def a_gagner(b, l):
             (b[0][2] == l and b[1][1] == l and b[2][0] == l))
 
 
-def whoStarts():
+def ordi_tour():
+    # actions de l'ordi
+    global button
+    move = get_ordi_choix()
+    print('move' + str(move))
+    button[move[0]][move[1]].config(state=DISABLED)
+    set_choix(move[0], move[1])
+
+
+# decide qui sera le premier joueur
+def qui_commence():
     global joueur
-    if (grille.count(' ') == 0):
-        test = askyesno(title="qui commence", message="Voulez-vous commencer ?")
+    global etat
+    etat = 'JEU'
+    if grille.count(' ') == 0:
+        test = askyesno(title="Qui commence?", message="Voulez-vous commencer?  [Oui : Humain, Non : Ordi]")
         if test is True:
             joueur = 0
         else:
             joueur = -1
-        print(joueur)
+            ordi_tour()
+
+    print(joueur)
 
 
-def set_choix(i, j, gb, l1, l2):
+def set_choix(i, j):
     global joueur
+    global button
+    global etat
 
     if grille[i][j] == ' ':
         if joueur % 2 == 0:
-            l1.config(state=DISABLED)
-            l2.config(state=ACTIVE)
             grille[i][j] = "X"
         else:
             button[i][j].config(state=ACTIVE)
-            l2.config(state=DISABLED)
-            l1.config(state=ACTIVE)
             grille[i][j] = "O"
 
         joueur += 1
         button[i][j].config(text=grille[i][j])
 
-    x = True
+    # x = True
 
     if a_gagner(grille, "X"):
         for i in range(len(grille)):
             for j in range(len(grille[i])):
                 if grille[i][j] == ' ':
                     button[i][j].config(state=DISABLED)
-        x = False
-        box = messagebox.showinfo("Winner", "Player won the match")
+        # x = False
+        etat = 'FIN'
+        box = messagebox.showinfo("Gagnant", "Le joueur a gagné")
 
     elif a_gagner(grille, "O"):
         for i in range(len(grille)):
             for j in range(len(grille[i])):
                 if grille[i][j] == ' ':
                     button[i][j].config(state=DISABLED)
-        x = False
-        box = messagebox.showinfo("Winner", "Computer won the match")
+        # x = False
+        etat = 'FIN'
+        box = messagebox.showinfo("Gagnant", "L'ordinateur a gagné")
 
-    elif (choix_valide()):
-        x = False
-        box = messagebox.showinfo("Tie Game", "Tie Game")
+    elif (partie_egalite()):
+        # x = False
+        box = messagebox.showinfo("Match nul", "Match nul")
+        etat = 'FIN'
 
-    if (x):
+    if etat == 'JEU':
         if joueur % 2 != 0:
-            move = get_ordi_choix()
-            print(move)
-            button[move[0]][move[1]].config(state=DISABLED)
-            set_choix(move[0], move[1], gb, l1, l2)
+            ordi_tour()
+
+    if etat == 'FIN':
+        box = messagebox.showinfo(";p", "Merci d'avoir joué!")
+        # exit()
 
 
-def event_reset():
+# reinitialise les variables globales et tableaux
+def raz():
+    global joueur
+    global etat
+    etat = 'INIT'
+    joueur = 0
     for i in range(len(grille)):
         for j in range(len(grille[i])):
             button[i][j].config(text=" ")
             button[i][j].config(state=ACTIVE)
             grille[i][j] = " "
-    whoStarts()
+    qui_commence()
 
 
-def choix_valide():
-    flag = True
+# verifie si la game est fini en egalite
+def partie_egalite():
+    egalite = True
     for i in grille:
         if (i.count(' ') > 0):
-            flag = False
-    return flag
+            egalite = False
+    return egalite
 
 
+# actions de l'ordi selon le IA choisi
 def get_ordi_choix():
     global ordiIA
     global joueur
@@ -121,6 +145,7 @@ def get_ordi_choix():
         messagebox.showinfo("", "selectionner une option")
 
 
+# AI heuristique
 def heuristique():
     possiblemove = []
     for i in range(len(grille)):
@@ -153,57 +178,65 @@ def heuristique():
             return edge[move]
 
 
+# AI hasard
 def hasard():
     lesChoix = []
-    choix = 0
-    choix2 = 0
+    choix = random.randint(0, 2)
+    choix2 = random.randint(0, 2)
+    lesChoix.append(choix)
+    lesChoix.append(choix2)
 
     while not grille[choix][choix2] == ' ':
+        lesChoix = []
         choix = random.randint(0, 2)
         choix2 = random.randint(0, 2)
         lesChoix.append(choix)
         lesChoix.append(choix2)
-        return lesChoix
+        # print('Hasard : ' + lesChoix)
+    return lesChoix
 
 
+# obtenir mouvements possibles
 def get_possible_moves():
     possibleMoves = []
-    for x in range(0, 3):
-        for y in range(0, 3):
-            if grille[x][y] == " ":
-                possibleMoves.append((x, y))
+    for i in range(len(grille)):
+        for j in range(len(grille[i])):
+            if grille[i][j] == ' ':
+                possibleMoves.append([i, j])
+                print(possibleMoves)
     return possibleMoves
 
 
-def make_move(coordinates):
+def make_move(move):
     global moves
-    x = coordinates[0]
-    y = coordinates[1]
+    if (grille[move[0]][move[1]] == " "):
+        moves.append(move[0])
+        moves.append(move[1])
 
-    if (grille[x][y] == " "):
-        moves.append(coordinates)
+
+def undo():
+    global moves
+    lastMove = moves.pop()
+    if lastMove:
+        grille[lastMove[0]][lastMove[1]] = " "
 
 
 def minimax(isMaxTurn, maximizerMark):
-    global moves
     scores = []
-
+    print(get_possible_moves())
     for move in get_possible_moves():
         make_move(move)
         scores.append(minimax(not isMaxTurn, maximizerMark))
-        lastMove = moves.pop()
-        if lastMove:
-            grille[lastMove[0]][lastMove[1]] = " "
-        if (isMaxTurn and max(scores) == 1) or (not isMaxTurn and min(scores) == -1):
-            break
+
+        undo()
+
     return max(scores) if isMaxTurn else min(scores)
 
 
-def gameboard_pc(game_board, l1, l2):
+# genère la grille de jeux
+def gameboard_pc(game_board):
+    print('Creating Buttons...')
     global button
-    global reponse
-    button = []
-
     for i in range(3):
         m = 3 + i
         button.append(i)
@@ -212,32 +245,36 @@ def gameboard_pc(game_board, l1, l2):
         for j in range(3):
             n = j
             button[i].append(j)
-            get_t = partial(set_choix, i, j, game_board, l1, l2)
+            get_t = partial(set_choix, i, j)
             button[i][j] = Button(game_board, command=get_t, bd=5, height=5, width=10)
             button[i][j].grid(row=m, column=n)
+
+    global etat
+    if (etat == 'INIT'):
+        qui_commence()
 
     game_board.mainloop()
 
 
+# commence le jeu
 def play():
     global ordiIA
     game_board = Tk()
+    global v
+    v = IntVar()
+    v.set(2)  # valeur par default
 
     game_board.title("Tic Tac Toe")
     l1 = Button(game_board, text="Player : X", width=10)
     l1.grid(row=2, column=0)
     l2 = Button(game_board, text="Computer : O",
-                width=10, state=DISABLED)
+                width=10)
 
     l2.grid(row=2, column=1)
-    play_again_button = Button(game_board, text='Nouvelle partie', font=('Arial', 10), command=event_reset)
+    play_again_button = Button(game_board, text='Nouvelle partie', font=('Arial', 10), command=raz)
     play_again_button.grid(row=2, column=2)
 
-    global v
-    v = IntVar()
-    v.set(2)  # valeur par default
-
-    r1 = Radiobutton(game_board, text="heuristoque", variable=v, value=1)
+    r1 = Radiobutton(game_board, text="heuristique", variable=v, value=1)
     r2 = Radiobutton(game_board, text="hasard", variable=v, value=2)
     r3 = Radiobutton(game_board, text="minimax", variable=v, value=3)
 
@@ -245,10 +282,10 @@ def play():
     r2.grid(row=0, column=1)
     r3.grid(row=0, column=2)
 
-    whoStarts()
-    gameboard_pc(game_board, l1, l2)
+    print('Will create buttons...')
+    gameboard_pc(game_board)
 
 
-# Call main function
+# methode main
 if __name__ == '__main__':
     play()
